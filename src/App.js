682c31b7f1a2b20d6daf7a45ba5da9e38b312fcc
LilianKalkman <liliankalkman@hotmail.com>
2018-02-23 14:08:32 +0100

    set product state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import SampleProducts from './sample-products';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      products: {},
+      order: {},
+    }
+
+    this.loadSampleProducts = this.loadSampleProducts.bind(this);
+  }
+
+  componentDidMount(){
+    this.loadSampleProducts();
+  }
+
+  loadSampleProducts(){
+    this.setState({
+      products: SampleProducts
+    });
+  }
+
   render() {
     return (
       <div>
+        <div className="products">{this.state.products.name}</div>
         <div>
           <Link to="/" type="button" className="btn btn-primary">Back</Link>
         </div>
---
+++ src/sample-products.js
@@ -0,0 +1,19 @@
+const SampleProducts = {
+  product1: {
+    name: 'Yogabroek wijde pijp',
+    image: 'https://media.istockphoto.com/photos/grilled-halibut-with-spinach-leeks-and-pine-nuts-picture-id503337620?k=6&m=503337620&s=612x612&w=0&h=C2RuTj4IDKvEvkC0J2i_7x2rnMlspq8CTcVVtdqd5u8=',
+    desc: 'Everyones favorite white fish. We will cut it to the size you need and ship it.',
+    price: 39,
+    status: 'available'
+  },
+
+  product2: {
+    name: 'Yogabroek strak',
+    image: 'https://i.istockimg.com/file_thumbview_approve/32135274/5/stock-photo-32135274-cooked-lobster.jpg',
+    desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
+    price: 32,
+    status: 'available'
+  },
+}
+
+export default SampleProducts;
